d91273138e28cbd16ee9639c6387986088b7582e
hellcoderz <sagar.vikrant@gmail.com>
2013-08-07 00:15:22 -0400

    google search using scraping

--- tests.py
+++
@@ -1,9 +0,0 @@
-tests = [
-	"watch avatar",
-	"tune to fox channel",
-	"find about mindy kaling",
-	"watch dr who",
-	"more movies like batman begins",
-	"show all simpsons",
-	"watch lord of rings "
-]
